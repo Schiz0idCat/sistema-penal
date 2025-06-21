aec119cb21bf114900536bf8cb89c2a8784856f3
Agustín <agustin.guzman.v@proton.me>
2025-06-21 09:34:04 -0400

    operador || mal puesto

--- main.c
+++ main.c
@@ -1530,7 +1530,7 @@ int interaccionInputDiligencia(struct Diligencia *diligencia, struct Persona **j
             default:
                 printf("\nOpción inválida. Intente de nuevo.\n");
         }
-    } while (opcion < 1 | opcion > 3);
+    } while (opcion < 1 || opcion > 3);
     return 0; // se logró agregar un responsable
 }
 
